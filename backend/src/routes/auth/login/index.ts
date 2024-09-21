bae4637446ff2e22ec7ea4b8113876bc8de15460
mele-dev <juantanca11@icloud.com>
2024-09-21 18:11:35 -0300

    TODO ROTO

---
+++ backend/src/routes/auth/login/index.ts
@@ -0,0 +1,45 @@
+import { Type } from "@sinclair/typebox";
+import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import { UserSchema } from '../../types/user.js';
+import { query } from "../../services/database.js";
+
+const tokenSchema = Type.Object({
+    jwtToken: Type.String(),
+});
+
+const auth: FastifyPluginAsyncTypebox = async (fastify, opts) => {
+    fastify.post("/", {
+        schema: {
+            body: Type.Object({
+                email: UserSchema.properties.email,
+                password: UserSchema.properties.password,
+            }),
+            response: {
+                200: tokenSchema,
+                //404: Type.Ref(ErrorMessageSchema),
+            },
+            security: [],
+        },
+
+        handler: async function(request, reply) {
+            const result = await query(
+                `
+                SELECT id
+                FROM users
+                WHERE email = $1
+                AND password = crypt($2, password);
+            `,
+                //[request.body.email, request.body.password],
+            );
+            if (result.rowCount !== 1) {
+                return reply
+                    .code(404)
+                    .send({ errorMessage: "Wrong email or password" });
+            }
+            const token = fastify.jwt.sign({ id: result.rows[0].id });
+            return reply.code(200).send({ jwtToken: token });
+        },
+    });
+};
+
+export default auth;
--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,7 +1,28 @@
 import { Static, Type } from "@sinclair/typebox";
 
-export const UserSchema = Type.Object({
-    id: Type.Number({ minimum: 1 }),
-    name: Type.String({ minLength: 3, maxLength: 20 }),
-    username: Type.String({ minLength: 4, maxLength: 20 })
-});
+export const UserSchema = Type.Object(
+    {
+        id: Type.Number({}),
+        username: Type.String({ minLength: 3, maxLength: 20 }),
+        email: Type.String({
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.source,
+            maxLength: 254
+        }),
+        password: Type.String({ minLength: 3, maxLength: 20 }),
+        spotify_id: Type.Number({}),
+        profile_picture_id: Type.Number({}),
+        name: Type.String({ maxLength: 25 }),
+    }
+);
+
+export const ErrorMessageSchema = Type.Object(
+    {
+        errorMessage: Type.String(),
+    },
+    {
+        title: "An error message",
+        $id: "errorMessage",
+    },
+);
+
+export type UserType = Static<typeof UserSchema>;
