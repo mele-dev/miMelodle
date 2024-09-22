8a994a6f5274e20dbb2af93c97ad3b21b84d2fdc
Cristian <CrisRPia>
2024-09-22 19:34:19 -0300

    it compiles

--- backend/src/plugins/sensible.ts
+++ backend/src/plugins/sensible.ts
@@ -1,11 +1,11 @@
 import fp from 'fastify-plugin'
-import sensible, { FastifySensibleOptions } from '@fastify/sensible'
+import sensible, { SensibleOptions } from '@fastify/sensible'
 
 /**
  * This plugins adds some utilities to handle http errors
  *
  * @see https://github.com/fastify/fastify-sensible
  */
-export default fp<FastifySensibleOptions>(async (fastify) => {
+export default fp<SensibleOptions>(async (fastify) => {
   fastify.register(sensible)
 })
