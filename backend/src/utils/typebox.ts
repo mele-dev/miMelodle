6f3f096b45b68bf8384eab7a6b40cc33c8d67c6a
Cristian <CrisRPia>
2024-09-25 02:16:52 -0300

    added a nicer way to define errors

--- backend/src/utils/typebox.ts
+++ backend/src/utils/typebox.ts
@@ -1,13 +1,21 @@
 import {
     Static,
+    TLiteral,
     TObject,
     TOmit,
     TPick,
     TSchema,
+    TString,
     Type,
 } from "@sinclair/typebox";
 import { Value } from "@sinclair/typebox/value";
 import schemaReferences from "../types/schemaReferences.js";
+import {
+    CommonErrorCode,
+    CommonErrorName,
+    commonErrors,
+    CommonErrorToCode,
+} from "./errors.js";
 
 // Redeclarations
 const safeTypeOverrides = {
@@ -26,12 +34,12 @@ const safeTypeOverrides = {
      * (From an override)
      */
     Omit<
-        TObj extends TObject,
-        TProperties extends (keyof TObj["properties"])[] & string[],
-    >(value: TObj, keys: TProperties): TOmit<TObj, TProperties> {
+        TTSchema extends TSchema,
+        TProperties extends (keyof TTSchema["properties"])[] & string[],
+    >(value: TTSchema, keys: TProperties): TOmit<TTSchema, TProperties> {
         return Type.Omit(value, keys);
     },
-} as const satisfies { [K in keyof typeof Type]?: unknown };
+} as const satisfies { [_ in keyof typeof Type]?: unknown };
 
 // Added functionality
 const helpers = {
@@ -41,15 +49,64 @@ const helpers = {
      */
     WithExamples<TTSchema extends TSchema>(
         schema: TTSchema,
-        examples: Static<TTSchema>[],
+        examples: Static<TTSchema>[]
     ) {
-        const output = Type.Union([schema], {...schema, examples});
+        const output = Type.Union([schema], { ...schema, examples });
 
         for (const example of output.examples) {
             Value.Assert(schema, schemaReferences, example);
         }
 
         return output;
+    },
+    /**
+     * @summary
+     * Returns an object mapping the code of every error name (or code) passed,
+     * to an appropriate error message schema, followin the standards of
+     * fastifySensible.
+     * @example
+     * ; // ... In a route schema.
+     * responses {
+     *     200: SafeType.Any() // You still have to specify successfull schemas!
+     *     // The line below automatically maps 400 and 404
+     *     ...SafeType.CreateErrors(["badRequest", "notFound"]),
+     *     // You could also write it like:
+     *     ...SafeType.CreateErrors([400, 404]);
+     * }
+     */
+    CreateErrors<TErrors extends (CommonErrorCode | CommonErrorName)[]>(
+        errors: TErrors
+    ): {
+        [E in TErrors[number] as CommonErrorToCode<E>]: TObject<{
+            statusCode: TLiteral<CommonErrorToCode<E>>;
+            error: TString;
+            message: TString;
+        }>;
+    } {
+        type ErrorSchema = TObject<{
+            statusCode: TLiteral<CommonErrorCode>;
+            error: TString;
+            message: TString;
+        }>;
+
+        const output = errors.reduce(
+            (acc, error) => {
+                const code =
+                    typeof error === "string" ? commonErrors[error] : error;
+                const schema = SafeType.Object({
+                    statusCode: Type.Literal(code),
+                    error: Type.String(),
+                    message: Type.String(),
+                }) satisfies ErrorSchema;
+
+                return { ...acc, [code]: schema };
+            },
+            {} as Record<CommonErrorCode, ErrorSchema>
+        );
+
+        // I'm pretty sure this cast is necessary. If this function is buggy,
+        // blame this. - cr
+        return output as any;
     },
 } as const;
 
