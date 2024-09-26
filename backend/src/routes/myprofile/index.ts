5dc19251f43db1a39a4d7630dba1a16447024aaf
Cristian <CrisRPia>
2024-09-26 00:48:32 -0300

    it now compiles (most of the lines are just formatting)

--- backend/src/routes/myprofile/index.ts
+++ backend/src/routes/myprofile/index.ts
@@ -1,102 +1,112 @@
-import { Type } from "@sinclair/typebox";
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { query, runPreparedQuery } from "../../services/database.js";
-import { deleteUser, selectUsers, updateUser } from "../../queries/dml.queries.js";
-import fastify from "fastify";
 import { SafeType } from "../../utils/typebox.js";
-import { MelodleUserType, UserSchema } from "../../types/user.js";
-import { UserType } from "@fastify/jwt";
-import { request } from "http";
+import { jwtTokenContentSchema, userSchema } from "../../types/user.js";
+import { deleteUser, updateUser } from "../../queries/dml.queries.js";
+import { sendError } from "../../utils/errors.js";
+import { MelodleTagNames } from "../../plugins/swagger.js";
+import { Value } from "@sinclair/typebox/value";
 
-const profile : FastifyPluginAsyncTypebox = async(fastify, opts) => {
-    fastify.get('/:username', {
+const profile: FastifyPluginAsyncTypebox = async (fastify, opts) => {
+    fastify.get("/:username", {
         onRequest: fastify.authenticate,
-        schema:{
+        schema: {
             params: SafeType.Object({
-                username: UserSchema.properties.username
-            })
+                username: userSchema.properties.username,
+            }),
+            tags: ["Melodle"] satisfies MelodleTagNames[]
         },
 
         async handler(request, reply) {
-            const userProfile= await query(`SELECT * FROM users WHERE username=$1;`,[request.params.username])
+            const userProfile = await query(
+                `SELECT * FROM users WHERE username=$1;`,
+                [request.params.username]
+            );
             return reply.code(200).send(userProfile);
-        }
+        },
     });
 
-    fastify.put('/:username', {
+    fastify.put("/:username", {
         onRequest: fastify.authenticate,
-        schema:{
-            params: SafeType.WithExamples(SafeType.Object({
-                username: UserSchema.properties.username
-            }), [
-                {
-                    username: 'cristina'
-                }
-            ]),
-            body: SafeType.WithExamples(SafeType.Omit(
-                    UserSchema,
-                    ["id", 'spotifyId']
-                ), 
-                    [
-                        {
-                            username: "juanchoTanca",
-                            email: "juanchoTanca@gmail.com",
-                            name: "juancho",
-                            password: "Juancho123!",
-                            profilePictureId: 1
-                        }
-                    ]
-                ),
+        schema: {
+            params: SafeType.WithExamples(
+                SafeType.Object({
+                    username: userSchema.properties.username,
+                }),
+                [
+                    {
+                        username: "cristina",
+                    },
+                ]
+            ),
+            body: SafeType.WithExamples(
+                SafeType.Omit(userSchema, ["id", "spotifyId"]),
+                [
+                    {
+                        username: "juanchoTanca",
+                        email: "juanchoTanca@gmail.com",
+                        name: "juancho",
+                        password: "Juancho123!",
+                        profilePictureId: 1,
+                    },
+                ]
+            ),
             response: {
-                200: SafeType.Omit(UserSchema, ['id', 'spotifyId']),
-                ...SafeType.CreateErrors([401]),
+                200: SafeType.Omit(userSchema, ["id", "spotifyId"]),
+                ...SafeType.CreateErrors(["unauthorized"]),
             },
+            tags: ["Melodle"] satisfies MelodleTagNames[]
         },
-        handler: async function(request, reply) {
-
-            const queryResult = await runPreparedQuery(updateUser,{
+        handler: async function (request, reply) {
+            const queryResult = await runPreparedQuery(updateUser, {
                 ...request.body,
-                oldUsername: request.params.username
-            })
+                oldUsername: request.params.username,
+            });
 
             if (queryResult.length !== 1) {
-                return reply.unauthorized("You cannot modify someone else.");
+                return sendError(
+                    reply,
+                    "unauthorized",
+                    "You cannot modify someone else."
+                );
             }
 
             return reply.code(200).send({
                 ...request.body,
             });
-
-        }
-
+        },
     });
 
-    fastify.delete('/:username', {
+    fastify.delete("/:username", {
         onRequest: fastify.authenticate,
         schema: {
-            params: SafeType.Pick(UserSchema, ['username']),
+            params: SafeType.Pick(userSchema, ["username"]),
             response: {
-                200: SafeType.Pick(UserSchema, ['username']),
+                200: SafeType.Pick(userSchema, ["username"]),
                 ...SafeType.CreateErrors([401]),
             },
+            tags: ["Melodle"] satisfies MelodleTagNames[]
         },
-        handler: async function(request, reply) {
-            const userToDelete = request.user as {
-                id: number
-            };
+        handler: async function (request, reply) {
+            Value.Assert(jwtTokenContentSchema, request.user);
 
-            const queryResult = await runPreparedQuery(deleteUser, userToDelete);
+            const queryResult = await runPreparedQuery(
+                deleteUser,
+                request.user
+            );
 
             switch (queryResult.length) {
                 case 0:
-                    return reply.unauthorized("Could not delete person with such crentials.");
+                    return reply.unauthorized(
+                        "Could not delete person with such crentials."
+                    );
                 case 1:
-                    return reply
-                        .code(200)
+                    return reply.code(200);
                 default:
                     throw `Deleted ${queryResult.length} rows.`;
             }
         },
-}   )}
+    });
+};
 
 export default profile;
