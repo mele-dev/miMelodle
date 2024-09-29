0f53aeda891db1d32ce6cbaa1ebd9c06692684c3
mele-dev <juantanca11@icloud.com>
2024-09-29 16:09:32 -0300

    asd

---
+++ backend/src/routes/artists/index.ts
@@ -0,0 +1,75 @@
+import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import {
+    artistSchema,
+} from "../../../types/artists.js";
+import { runPreparedQuery } from "../../../services/database.js";
+import { SafeType } from "../../utils/typebox.js";
+import { insertUser } from "../../queries/dml.queries.js";
+import { sendError } from "../../../utils/errors.js";
+import { MelodleTagNames } from "../../../plugins/swagger.js";
+
+const auth: FastifyPluginAsyncTypebox = async (fastify, _opts) => {
+    fastify.post("", {
+        schema: {
+            body: SafeType.WithExamples(
+                SafeType.Pick(userSchema, [
+                    "username",
+                    "profilePictureId",
+                    "email",
+                    "password",
+                    "name",
+                ]),
+                [
+                    {
+                        username: "juanalopez1",
+                        email: "juanaxlopez1@gmail.com",
+                        name: "juana",
+                        password: "Juana123!",
+                        profilePictureId: 1,
+                    },
+                ]
+            ),
+            response: {
+                200: SafeType.WithExamples(
+                    SafeType.Object({
+                        ...jwtTokenSchema.properties,
+                        ...SafeType.Pick(userSchema, ["id"]).properties,
+                    }),
+                    [
+                        {
+                            jwtToken:
+                                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNzI3NDExODc4fQ.lCYmZF_REl8rYYj1UjJzacXrPCTyjVdA-KsR71xHwQw",
+                            id: 2,
+                        },
+                    ]
+                ),
+                ...SafeType.CreateErrors(["badRequest"]),
+            },
+            security: [],
+            tags: ["Auth", "User CRUD"] satisfies MelodleTagNames[],
+            summary: "Create a user.",
+            description:
+                "Creates a new user with the given credentials if possible.",
+        },
+
+        handler: async function (request, reply) {
+            const result = await runPreparedQuery(insertUser, request.body);
+
+            if (result.length !== 1) {
+                return sendError(
+                    reply,
+                    "badRequest",
+                    "No se pudo crear el usuario."
+                );
+            }
+
+            const token = fastify.jwt.sign({
+                id: result[0].id,
+            } satisfies JwtTokenContent);
+
+            return reply.code(200).send({ jwtToken: token, id: result[0].id });
+        },
+    });
+};
+
+export default auth;
