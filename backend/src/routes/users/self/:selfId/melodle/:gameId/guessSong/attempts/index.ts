def30df6055ce664a4daca0a4ea9d4cb6a9a59d2
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-09-29 21:10:51 -0300

    attempt route ?

--- backend/src/routes/users/self/:selfId/melodle/:gameId/guessSong/attempts/index.ts
+++ backend/src/routes/users/self/:selfId/melodle/:gameId/guessSong/attempts/index.ts
@@ -1,18 +1,24 @@
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { SafeType } from "../../../../../../../../utils/typebox.js";
 import { MelodleTagNames } from "../../../../../../../../plugins/swagger.js";
+import { MelodleAttemptSchema } from "../../../../../../../../types/melodle.js";
 
 export default (async (fastify) => {
     fastify.post("", {
         onRequest: [],
         schema: {
+            body: MelodleAttemptSchema,
             response: {
-                200: SafeType.Literal("TODO!"),
+                200: SafeType.Literal("Success"),
                 ...SafeType.CreateErrors([]),
             },
             summary: "Submit a guess for a melodle game.",
-            description: undefined,
+            description: "Submit user attempts for guessing the song in a melodle game.",
             tags: ["Juana"] satisfies MelodleTagNames[],
+            params: SafeType.Object({
+                selfId: SafeType.String({ description: "ID of the user" }),
+                gameId: SafeType.String({ description: "ID of the melodle game" })
+            })
         },
         async handler(_request, reply) {
             return reply.notImplemented();
