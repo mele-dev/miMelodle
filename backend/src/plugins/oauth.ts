b9118ed94ee5c33fa4a732692d171d1a9f158dfa
Cristian <CrisRPia>
2024-10-09 01:09:36 -0300

    The register menu appears and redirects fine

---
+++ backend/src/plugins/oauth.ts
@@ -0,0 +1,58 @@
+import fastifyOauth2 from "@fastify/oauth2";
+import fastifyPlugin from "fastify-plugin";
+
+type SpotifyScopes =
+    // Images
+    | "ugc-image-upload"
+    // Spotify Connect
+    | "user-read-playback-state"
+    | "user-modify-playback-state"
+    | "user-read-currently-playing"
+    // Playback
+    | "app-remote-control"
+    | "streaming"
+    // Playlists
+    | "playlist-read-private"
+    | "playlist-read-collaborative"
+    | "playlist-modify-private"
+    | "playlist-modify-public"
+    // Follow
+    | "user-follow-modify"
+    | "user-follow-read"
+    // Listening History
+    | "user-read-playback-position"
+    | "user-top-read"
+    | "user-read-recently-played"
+    // Library
+    | "user-library-modify"
+    | "user-library-read"
+    // Users
+    | "user-read-email"
+    | "user-read-private"
+    // Open Access
+    | "user-soa-link"
+    | "user-soa-unlink"
+    | "soa-manage-entitlements"
+    | "soa-manage-partner"
+    | "soa-create-partner";
+
+export default fastifyPlugin(async (fastify) => {
+    fastify.register(fastifyOauth2, {
+        name: "spotifyRegister",
+        scope: [
+            "user-read-email",
+            "user-read-private",
+        ] satisfies SpotifyScopes[],
+        pkce: "S256",
+        credentials: {
+            client: {
+                // FIXME: Move this info to .env
+                id: "1cbcb63c89134289aac6425996162c4a",
+                secret: "f2903f0fa6da4e17b8bb324b86b12940",
+            },
+            auth: fastifyOauth2.fastifyOauth2.SPOTIFY_CONFIGURATION,
+        },
+        startRedirectPath: "/auth/register/spotify",
+        callbackUri: "https://localhost/backend/auth/register/spotify/callback",
+    });
+});
--- backend/src/types/fastify.ts
+++ backend/src/types/fastify.ts
@@ -3,5 +3,6 @@ import { OAuth2Namespace } from '@fastify/oauth2';
 declare module 'fastify' {
     interface FastifyInstance{
         googleOAuth2: OAuth2Namespace;
+        oauth2SpotifyRegister: OAuth2Namespace;
     }
 }
