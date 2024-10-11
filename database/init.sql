c0f959c14646e178bfc1942beca3cc93acca0011
Cristian <CrisRPia>
2024-10-11 00:22:00 -0300

    Added routes to fake large numbers of users

--- database/init.sql
+++ database/init.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE "profilePictures" (
-    id        SERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     "filename" TEXT NOT NULL
 );
 
@@ -15,24 +15,24 @@ CREATE DOMAIN username_domain AS VARCHAR(20) CHECK ( LENGTH(value) >= 3 );
 
 CREATE TABLE users (
     id                 SERIAL PRIMARY KEY,
-    username           username_domain UNIQUE                            NOT NULL,
+    username           username_domain UNIQUE                             NOT NULL,
     -- https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
-    email              email_domain UNIQUE                               NOT NULL,
-    "passwordHash"      TEXT                                              NULL,
-    "spotifyId"         TEXT UNIQUE                                       NULL,
+    email              email_domain UNIQUE                                NOT NULL,
+    "passwordHash"     TEXT                                               NULL,
+    "spotifyId"        TEXT UNIQUE                                        NULL,
     "profilePictureId" BIGINT REFERENCES "profilePictures" (id) DEFAULT 0 NOT NULL,
-    name               VARCHAR(25)                                       NOT NULL,
+    name               VARCHAR(25)                                        NOT NULL,
     CHECK ( "passwordHash" IS NOT NULL OR "spotifyId" IS NOT NULL )
 );
 
 CREATE TYPE "friendshipStatus" AS ENUM ('pending', 'accepted', 'blocked');
 
 CREATE TABLE friendships (
-    id         SERIAL PRIMARY KEY,
-    "userId"    BIGINT REFERENCES users (id)        NOT NULL,
-    "friendId"  BIGINT REFERENCES users (id)        NOT NULL,
-    "createdAt" timestamptz       DEFAULT NOW()     NOT NULL,
-    status     "friendshipStatus" DEFAULT 'pending' NOT NULL,
+    id          SERIAL PRIMARY KEY,
+    "userId"    BIGINT REFERENCES users (id)         NOT NULL,
+    "friendId"  BIGINT REFERENCES users (id)         NOT NULL,
+    "createdAt" timestamptz        DEFAULT NOW()     NOT NULL,
+    status      "friendshipStatus" DEFAULT 'pending' NOT NULL,
     CHECK ( "userId" <> friendships."friendId" )
 );
 
@@ -42,7 +42,7 @@ CREATE TABLE artists (
 );
 
 CREATE TABLE saved_artists (
-    id          SERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     "userId"     BIGINT REFERENCES users (id)   NOT NULL,
     "artistId"   BIGINT REFERENCES artists (id) NOT NULL,
     "savedAt"    timestamptz DEFAULT NOW()      NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE saved_artists (
 );
 
 CREATE TABLE streaks (
-    id           SERIAL PRIMARY KEY,
+    id            SERIAL PRIMARY KEY,
     "userId"      BIGINT REFERENCES users (id) NOT NULL,
     "streakCount" BIGINT      DEFAULT 0        NOT NULL,
     "lastUpdated" timestamptz DEFAULT NOW()    NOT NULL,
@@ -58,22 +58,22 @@ CREATE TABLE streaks (
 );
 
 CREATE TABLE songs (
-    id            SERIAL PRIMARY KEY,
+    id             SERIAL PRIMARY KEY,
     "musixmatchId" TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE games (
-    id        SERIAL PRIMARY KEY,
-    "userId"   BIGINT REFERENCES users (id) NOT NULL,
-    "songId"   BIGINT REFERENCES songs (id) NOT NULL,
+    id       SERIAL PRIMARY KEY,
+    "userId" BIGINT REFERENCES users (id) NOT NULL,
+    "songId" BIGINT REFERENCES songs (id) NOT NULL,
     "gameId" DATE DEFAULT NOW()::DATE     NOT NULL
 );
 
 CREATE TABLE attempts (
     id              SERIAL PRIMARY KEY,
-    "gameId"         BIGINT REFERENCES games (id) NOT NULL,
+    "gameId"        BIGINT REFERENCES games (id) NOT NULL,
     "guessedSongId" BIGINT REFERENCES songs (id) NOT NULL,
-    "guessedAt"      timestamptz                  NOT NULL
+    "guessedAt"     timestamptz                  NOT NULL
 );
 
 CREATE TABLE "gameModes" (
@@ -90,14 +90,14 @@ SELECT *
   FROM "gameModes";
 
 CREATE TABLE "gameConfig" (
-    id        SERIAL PRIMARY KEY,
-    "userId"   BIGINT REFERENCES users (id)                NOT NULL,
+    id         SERIAL PRIMARY KEY,
+    "userId"   BIGINT REFERENCES users (id)                 NOT NULL,
     "gameMode" BIGINT DEFAULT 1 REFERENCES "gameModes" (id) NOT NULL
 );
 
 CREATE TABLE "artistGameConfig" (
     "gameConfigId" BIGINT REFERENCES "gameConfig" (id) NOT NULL,
-    "artistId"      BIGINT REFERENCES artists (id)     NOT NULL,
+    "artistId"     BIGINT REFERENCES artists (id)      NOT NULL,
     PRIMARY KEY ("gameConfigId", "artistId")
 );
 
