fb0a5f4ba04d1096025f01d3813e9e401b11ca4c
Cristian <CrisRPia>
2024-10-15 03:30:07 -0300

    First mockup of register page

---
+++ frontend/orval.config.ts
@@ -0,0 +1,22 @@
+export default {
+    spotify: {
+        input: {
+            target: "https://developer.spotify.com/reference/web-api/open-api-schema.yaml",
+        },
+        output: {
+            target: "src/apiCodegen/spotify.ts",
+            prettier: true,
+            baseUrl: 'https://api.spotify.com/v1',
+        },
+    },
+    backend: {
+        input: {
+            target: "./schemas/backendSchema.json",
+        },
+        output: {
+            target: "src/apiCodegen/backend.ts",
+            prettier: true,
+            baseUrl: "https://localhost/backend",
+        },
+    },
+};
--- frontend/src/app/register-page/register-page.translations.ts
+++ frontend/src/app/register-page/register-page.translations.ts
@@ -2,8 +2,8 @@ import { Translator } from "../../utils/language";
 
 export const registerTranslations = new Translator({
     title: {
-        en: "Create an account",
-        es: "Crear cuenta",
+        en: "Join melodle",
+        es: "Unirse a melodle",
     },
     existingAccount: {
         en: "Already have an account? Log in.",
@@ -12,5 +12,45 @@ export const registerTranslations = new Translator({
     howDoYouWantOthersToSeeYou: {
         en: "How do you want others to see you?",
         es: "¿Cómo quieres que te vean los demás?",
+    },
+    nameDescription: {
+        en: "A name for others to see",
+        es: "El nombre que verán los demás",
+    },
+    usernameDescription: {
+        en: "A username for others to search",
+        es: "Un identificador para buscarte",
+    },
+    namePlaceholder: {
+        en: "Your name",
+        es: "Tu nombre",
+    },
+    usernamePlaceholder: {
+        en: "Your-username",
+        es: "Tu-nombre-de-usuario",
+    },
+    weNeedSomeDataToIdentifyYou: {
+        en: "We need some data to identify you",
+        es: "Necesitamos algunos datos para identificarte",
+    },
+    email: {
+        en: "Email",
+        es: "Correo electrónico",
+    },
+    password: {
+        en: "Password",
+        es: "Contraseña",
+    },
+    confirmPassword: {
+        en: "Confirm password",
+        es: "Confirmar contraseña",
+    },
+    createAccountButton: {
+        en: "Create account",
+        es: "Crear cuenta",
+    },
+    orRegister: {
+        en: "Or register via",
+        es: "O regístrese via",
     },
 } as const);
