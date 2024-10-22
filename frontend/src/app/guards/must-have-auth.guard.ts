84796f90a3a943b4ea9594fe56f8abc84c64e6db
Cristian <CrisRPia>
2024-10-22 11:58:41 -0300

    forgot to add this

--- frontend/src/app/guards/must-have-auth.guard.ts
+++ frontend/src/app/guards/must-have-auth.guard.ts
@@ -2,14 +2,33 @@ import { CanActivateFn } from "@angular/router";
 import { LocalStorageService } from "../services/local-storage.service";
 import { inject } from "@angular/core";
 import { SafeRoutingService } from "../services/safe-routing.service";
+import axios from "axios";
 
-export const mustHaveAuthGuard: CanActivateFn = (_route, _state) => {
+/**
+ * Checks that there is an auth token stored in localStorage and adds an
+ * interceptor to add that token on every axios request.
+ */
+export const mustHaveAuthGuard: CanActivateFn = async (_route, _state) => {
     const localStorageService = inject(LocalStorageService);
     const safeRouterService = inject(SafeRoutingService);
 
-    if (localStorageService.getItem("userInfo") === null) {
+    const userInfo = localStorageService.getItem("userInfo");
+
+    if (!userInfo?.jwtToken) {
         safeRouterService.navigate(["/auth"]);
+        return false;
     }
-    // TODO: Ask the backend if the token is valid.
+
+    // If the user is already logged in, their token should be injected into
+    // every request to the backend.
+    axios.interceptors.request.use((config) => {
+        // Authorization may be overriden by setting it to anything other than
+        // undefined.
+        if (config.headers.Authorization === undefined) {
+            config.headers.Authorization = "Bearer " + userInfo.jwtToken;
+        }
+        return config;
+    });
+
     return true;
 };
