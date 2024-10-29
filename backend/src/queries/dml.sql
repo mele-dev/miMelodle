2bcd7baf8d6ea945cda20f43ec54320b5e701c9b
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-10-29 12:42:49 -0300

    Revert "conflicts resolved"
    This reverts commit 817da9e18d48879de168f6712c2522bc025f3141.

--- backend/src/queries/dml.sql
+++ backend/src/queries/dml.sql
@@ -4,10 +4,9 @@ FROM users;
 
 /* @name loginUser */
 SELECT id
-  FROM users
- WHERE email = :emailOrUsername! or username = :emailOrUsername!
-   AND check_password("passwordHash", :password!);
->>>>>>> main
+FROM users
+WHERE email = :email!
+  AND check_password("passwordHash", :password!);
 
 /* @name insertUser */
 INSERT
@@ -46,7 +45,6 @@ FROM users u
          INNER JOIN public."profilePictures" pp ON pp.id = u."profilePictureId"
 where u.id = :selfId!;
 
-
 /* @name insertIcon */
 INSERT
 INTO "profilePictures" (filename)
@@ -60,7 +58,6 @@ COMMIT;
 
 /* @name rollbackTransaction */
 ROLLBACK;
-
 
 /* @name getSelfFriends */
 SELECT f."user2Id",
@@ -134,36 +131,3 @@ where ((b."blockedUserId" = :selfId! and b."userWhoBlocksId" = :targetUserId!) o
 SELECT *
 from blocks b
 where (b."blockedUserId" = :selfId! and b."userWhoBlocksId" = :targetUserId!);
-
-/* @name insertUserSpotify */
-   INSERT
-     INTO users (username, email, "passwordHash", "spotifyId", "profilePictureId", name)
-   VALUES (:username!, :email!, default, :spotifyId!, default, :name!)
-RETURNING id;
-
-/* @name loginUserSpotify */
-SELECT u.id
-  FROM users u
- WHERE u."spotifyId" = :spotifyId!;
-
-/* @name searchForUserEmailOrUsername */
-SELECT u.username, u.email
-  FROM users u
- WHERE u.username = :username
-    OR u.email = :email;
-
-/* @name searchUser */
-  WITH similarity     AS (
-      SELECT u.*, similarity(u.username, :username!) AS "rank!" FROM users u
-  ),
-       filtered_users AS (
-      SELECT *
-        FROM similarity
-       WHERE "rank!" >= :rankThreshold!
-  )
-SELECT u.*, pp.filename AS "profilePictureFilename", CEIL(COUNT(*) OVER () / :pageSize!::FLOAT) AS "totalPages!"
-  FROM filtered_users u
-           INNER JOIN "profilePictures" pp ON u."profilePictureId" = pp.id
- ORDER BY "rank!" DESC, levenshtein(u.username, :username!)
- LIMIT :pageSize! OFFSET :pageSize!::INT * :page!::INT;
-
--- database/init.sql
+++ database/init.sql
@@ -4,39 +4,25 @@ CREATE SCHEMA public;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
 CREATE TABLE "profilePictures" (
-    id         SERIAL PRIMARY KEY,
-    "filename" TEXT UNIQUE NOT NULL
+    id        SERIAL PRIMARY KEY,
+    "filename" TEXT NOT NULL
 );
 
 CREATE DOMAIN email_domain AS VARCHAR(254) CHECK ( value ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$' );
 
-CREATE DOMAIN username_domain AS VARCHAR(50) CHECK ( LENGTH(value) >= 3 AND value ~ '^[a-zA-Z0-9\.-_]+$' );
-
-CREATE OR REPLACE FUNCTION get_default_profile_picture() RETURNS BIGINT AS $$
-DECLARE
-    default_id BIGINT;
-BEGIN
-    SELECT pp.id INTO default_id FROM "profilePictures" pp WHERE pp.filename = 'default.svg' LIMIT 1;
-
-    RETURN default_id;
-END;
-$$ LANGUAGE plpgsql;
+CREATE DOMAIN username_domain AS VARCHAR(20) CHECK ( LENGTH(value) >= 3 );
 
 CREATE TABLE users (
     id                 SERIAL PRIMARY KEY,
-    username           username_domain UNIQUE                   NOT NULL,
+    username           username_domain UNIQUE                            NOT NULL,
     -- https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
-    email              email_domain UNIQUE                      NOT NULL,
-    "passwordHash"     TEXT                                     NULL,
-    "spotifyId"        TEXT UNIQUE                              NULL,
-    "profilePictureId" BIGINT REFERENCES "profilePictures" (id) NOT NULL DEFAULT get_default_profile_picture(),
-    name               VARCHAR(25)                              NOT NULL,
-    CHECK ( ("passwordHash" IS NOT NULL OR "spotifyId" IS NOT NULL) AND LENGTH(name) >= 1 )
+    email              email_domain UNIQUE                               NOT NULL,
+    "passwordHash"      TEXT                                              NULL,
+    "spotifyId"         TEXT UNIQUE                                       NULL,
+    "profilePictureId" BIGINT REFERENCES "profilePictures" (id) DEFAULT 0 NOT NULL,
+    name               VARCHAR(25)                                       NOT NULL,
+    CHECK ( "passwordHash" IS NOT NULL OR "spotifyId" IS NOT NULL )
 );
 
 CREATE TYPE "friendshipStatus" AS ENUM ('pending', 'accepted');
@@ -55,10 +41,6 @@ CREATE TABLE blocks (
     "blockedUserId"    BIGINT REFERENCES users (id)        NOT NULL,
     "createdAt" timestamptz       DEFAULT NOW()     NOT NULL,
     PRIMARY KEY ("userWhoBlocksId", "blockedUserId")
-
-  -- Optimize username for fuzzy searching.
-CREATE INDEX users_username_trgm_idx ON users USING gin (username gin_trgm_ops);
-
 );
 
 CREATE TABLE artists (
@@ -67,7 +49,7 @@ CREATE TABLE artists (
 );
 
 CREATE TABLE saved_artists (
-    id           SERIAL PRIMARY KEY,
+    id          SERIAL PRIMARY KEY,
     "userId"     BIGINT REFERENCES users (id)   NOT NULL,
     "artistId"   BIGINT REFERENCES artists (id) NOT NULL,
     "savedAt"    timestamptz DEFAULT NOW()      NOT NULL,
@@ -75,7 +57,7 @@ CREATE TABLE saved_artists (
 );
 
 CREATE TABLE streaks (
-    id            SERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     "userId"      BIGINT REFERENCES users (id) NOT NULL,
     "streakCount" BIGINT      DEFAULT 0        NOT NULL,
     "lastUpdated" timestamptz DEFAULT NOW()    NOT NULL,
@@ -83,22 +65,22 @@ CREATE TABLE streaks (
 );
 
 CREATE TABLE songs (
-    id             SERIAL PRIMARY KEY,
+    id            SERIAL PRIMARY KEY,
     "musixmatchId" TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE games (
-    id       SERIAL PRIMARY KEY,
-    "userId" BIGINT REFERENCES users (id) NOT NULL,
-    "songId" BIGINT REFERENCES songs (id) NOT NULL,
+    id        SERIAL PRIMARY KEY,
+    "userId"   BIGINT REFERENCES users (id) NOT NULL,
+    "songId"   BIGINT REFERENCES songs (id) NOT NULL,
     "gameId" DATE DEFAULT NOW()::DATE     NOT NULL
 );
 
 CREATE TABLE attempts (
     id              SERIAL PRIMARY KEY,
-    "gameId"        BIGINT REFERENCES games (id) NOT NULL,
+    "gameId"         BIGINT REFERENCES games (id) NOT NULL,
     "guessedSongId" BIGINT REFERENCES songs (id) NOT NULL,
-    "guessedAt"     timestamptz                  NOT NULL
+    "guessedAt"      timestamptz                  NOT NULL
 );
 
 CREATE TABLE "gameModes" (
@@ -115,14 +97,14 @@ SELECT *
   FROM "gameModes";
 
 CREATE TABLE "gameConfig" (
-    id         SERIAL PRIMARY KEY,
-    "userId"   BIGINT REFERENCES users (id)                 NOT NULL,
+    id        SERIAL PRIMARY KEY,
+    "userId"   BIGINT REFERENCES users (id)                NOT NULL,
     "gameMode" BIGINT DEFAULT 1 REFERENCES "gameModes" (id) NOT NULL
 );
 
 CREATE TABLE "artistGameConfig" (
     "gameConfigId" BIGINT REFERENCES "gameConfig" (id) NOT NULL,
-    "artistId"     BIGINT REFERENCES artists (id)      NOT NULL,
+    "artistId"      BIGINT REFERENCES artists (id)     NOT NULL,
     PRIMARY KEY ("gameConfigId", "artistId")
 );
 
