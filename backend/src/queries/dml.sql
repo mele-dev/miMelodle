413bd906cd02a75b8ef5b6d8e11a283e63f00e93
Cristian <CrisRPia>
2024-10-29 14:54:20 -0300

    fix merge mistakes

--- backend/src/queries/dml.sql
+++ backend/src/queries/dml.sql
@@ -1,42 +1,42 @@
 /* @name selectUsers */
 SELECT *
-FROM users;
+  FROM users;
 
 /* @name loginUser */
 SELECT id
   FROM users
- WHERE email = :emailOrUsername! or username = :emailOrUsername!
-   AND check_password("passwordHash", :password!);
+ WHERE email = :emailOrUsername!
+    OR username = :emailOrUsername! AND check_password("passwordHash", :password!);
 
 /* @name insertUser */
-INSERT
-INTO users (username, email, "passwordHash", "spotifyId", "profilePictureId", name)
-VALUES (:username!, :email!, encrypt_password(:password!), default, :profilePictureId!, :name!)
+   INSERT
+     INTO users (username, email, "passwordHash", "spotifyId", "profilePictureId", name)
+   VALUES (:username!, :email!, encrypt_password(:password!), default, :profilePictureId!, :name!)
 RETURNING id;
 
 /* @name updateUser */
-UPDATE users
-SET username           = :username!
-  , email              = :email!
-  , "passwordHash"     = encrypt_password(:password!)
-  , "profilePictureId" = :profilePictureId!
-  , name               = :name!
-WHERE id = :selfId!
+   UPDATE users
+      SET username           = :username!
+        , email              = :email!
+        , "passwordHash"     = encrypt_password(:password!)
+        , "profilePictureId" = :profilePictureId!
+        , name               = :name!
+    WHERE id = :selfId!
 RETURNING username;
 
 /* @name deleteUser */
-DELETE
-FROM users
-WHERE id = :selfId!
+   DELETE
+     FROM users
+    WHERE id = :selfId!
 RETURNING *;
 
 /* @name selectAllIcons */
 SELECT *
-FROM "profilePictures";
+  FROM "profilePictures";
 
 /* @name deleteIcons */
-DELETE
-FROM "profilePictures"
+   DELETE
+     FROM "profilePictures"
 RETURNING 1 AS output;
 
 /* @name getSelfuser */
@@ -47,7 +47,7 @@ SELECT pp.filename AS "profilePictureFile", u.name, u.email, u.username, u.id
 
 /* @name insertIcon */
 INSERT
-INTO "profilePictures" (filename)
+  INTO "profilePictures" (filename)
 VALUES (:file!);
 
 /* @name beginTransaction */
@@ -60,78 +60,80 @@ COMMIT;
 ROLLBACK;
 
 /* @name getSelfFriends */
-SELECT f."user2Id",
-       f."userId",
-       f.status,
-       u.name       as name1,
-       u2.name      as name2,
-       u.username   as username1,
-       u2.username  as username2,
-       pP.id        as "profilePictureId1",
-       pP2.id       as "profilePictureId2",
-       pP.filename  as "profilePictureFilename1",
-       pP2.filename as "profilePictureFilename2"
-FROM friendships f
-         inner join public.users u2 on u2.id = f."user2Id"
-         inner join public.users u on u.id = f."userId"
-         inner join public."profilePictures" pP on pP.id = u."profilePictureId"
-         inner join public."profilePictures" pP2 on pP2.id = u2."profilePictureId"
-where "user2Id" = :selfId!
-   or "userId" = :selfId!;
+SELECT f."user2Id"
+     , f."userId"
+     , f.status
+     , u.name       AS name1
+     , u2.name      AS name2
+     , u.username   AS username1
+     , u2.username  AS username2
+     , pp.id        AS "profilePictureId1"
+     , pp2.id       AS "profilePictureId2"
+     , pp.filename  AS "profilePictureFilename1"
+     , pp2.filename AS "profilePictureFilename2"
+  FROM friendships f
+           INNER JOIN public.users u2 ON u2.id = f."user2Id"
+           INNER JOIN public.users u ON u.id = f."userId"
+           INNER JOIN public."profilePictures" pp ON pp.id = u."profilePictureId"
+           INNER JOIN public."profilePictures" pp2 ON pp2.id = u2."profilePictureId"
+ WHERE "user2Id" = :selfId!
+    OR "userId" = :selfId!;
 
 /* @name deleteFriend */
-delete
-from friendships f
-where (f."userId" = :selfId! and f."user2Id" = :targetUserId!)
-   or (f."userId" = :targetUserId! and f."user2Id" = :selfId!)
+   DELETE
+     FROM friendships f
+    WHERE (f."userId" = :selfId! AND f."user2Id" = :targetUserId!)
+       OR (f."userId" = :targetUserId! AND f."user2Id" = :selfId!)
 RETURNING *;
 
 /* @name addNewFriend */
-insert into friendships ("userId", "user2Id")
-values (:selfId!, :targetUserId!)
+   INSERT
+     INTO friendships ("userId", "user2Id")
+   VALUES (:selfId!, :targetUserId!)
 RETURNING status;
 
 /* @name acceptRequest */
-update friendships f
-set status = 'accepted'
-where (f."userId" = :selfId! and f."user2Id" = :targetUserId!)
-   or (f."userId" = :targetUserId! and f."user2Id" = :selfId!);
+UPDATE friendships f
+   SET status = 'accepted'
+ WHERE (f."userId" = :selfId! AND f."user2Id" = :targetUserId!)
+    OR (f."userId" = :targetUserId! AND f."user2Id" = :selfId!);
 
 /* @name getStatus */
 SELECT status
-FROM friendships f
-where (f."userId" = :selfId! and f."user2Id" = :targetUserId!)
-   or (f."userId" = :targetUserId! and f."user2Id" = :selfId!);
+  FROM friendships f
+ WHERE (f."userId" = :selfId! AND f."user2Id" = :targetUserId!)
+    OR (f."userId" = :targetUserId! AND f."user2Id" = :selfId!);
 
 /* @name blockUser */
-INSERT INTO blocks("userWhoBlocksId", "blockedUserId")
-values (:selfId!, :targetUserId!)
+   INSERT
+     INTO blocks("userWhoBlocksId", "blockedUserId")
+   VALUES (:selfId!, :targetUserId!)
 RETURNING *;
 
 /* @name unblockUser */
-DELETE
-FROM blocks
-WHERE "userWhoBlocksId" = :selfId!
-  and "blockedUserId" = :targetUserId!
+   DELETE
+     FROM blocks
+    WHERE "userWhoBlocksId" = :selfId!
+      AND "blockedUserId" = :targetUserId!
 RETURNING *;
 
 /* @name getRequestReceiver */
 SELECT "user2Id"
-FROM friendships f
-where (f."userId" = :selfId! and f."user2Id" = :targetUserId!)
-   or (f."userId" = :targetUserId! and f."user2Id" = :selfId!);
+  FROM friendships f
+ WHERE (f."userId" = :selfId! AND f."user2Id" = :targetUserId!)
+    OR (f."userId" = :targetUserId! AND f."user2Id" = :selfId!);
 
-/* @name isUserBlocked */ 
+/* @name isUserBlocked */
 SELECT *
-from blocks b
-where ((b."blockedUserId" = :selfId! and b."userWhoBlocksId" = :targetUserId!) or
-       (b."blockedUserId" = :targetUserId! and b."userWhoBlocksId" = :selfId!));
+  FROM blocks b
+ WHERE ((b."blockedUserId" = :selfId! AND b."userWhoBlocksId" = :targetUserId!) OR
+        (b."blockedUserId" = :targetUserId! AND b."userWhoBlocksId" = :selfId!));
 
 /* @name blockAlreadyExists */
 SELECT *
-from blocks b
-where (b."blockedUserId" = :selfId! and b."userWhoBlocksId" = :targetUserId!);
-=======
+  FROM blocks b
+ WHERE (b."blockedUserId" = :selfId! AND b."userWhoBlocksId" = :targetUserId!);
+
 /* @name insertUserSpotify */
    INSERT
      INTO users (username, email, "passwordHash", "spotifyId", "profilePictureId", name)
@@ -163,4 +165,3 @@ SELECT u.*, pp.filename AS "profilePictureFilename", CEIL(COUNT(*) OVER () / :pa
            INNER JOIN "profilePictures" pp ON u."profilePictureId" = pp.id
  ORDER BY "rank!" DESC, levenshtein(u.username, :username!)
  LIMIT :pageSize! OFFSET :pageSize!::INT * :page!::INT;
->>>>>>> main
