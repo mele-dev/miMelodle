ea7706345eeb9810e1cfe40ca1482a17ff6cc646
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-11-10 17:19:40 -0300

    changes

---
+++ frontend/src/app/components/artist-finder/artist-finder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-finder',
+  standalone: true,
+  imports: [],
+  templateUrl: './artist-finder.component.html',
+  styleUrl: './artist-finder.component.css'
+})
+export class ArtistFinderComponent {
+
+}
