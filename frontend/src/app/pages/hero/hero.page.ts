28df925522b8eff574d24919622490c426acde7b
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-11-22 14:49:28 -0300

    component

---
+++ frontend/src/app/pages/hero/hero.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.page.html',
+  styleUrl: './hero.page.css'
+})
+export class HeroPage {
+
+}
